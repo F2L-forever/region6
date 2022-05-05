f1ed02b9844d85879b268e155203bb626dc8d6fd
ahl <1297892806@qq.com>
2022-05-06 00:54:26 +0800

    fix

--- src/main/java/com/ringtop/region6/WormCore.java
+++ src/main/java/com/ringtop/region6/WormCore.java
@@ -20,35 +20,43 @@ public class WormCore {
 
     public void Wormcore(LinkedBlockingQueue<String> UrlQueue, String sqlFile) throws InterruptedException, IOException {
         synchronized (this) {
+
+
             if (!UrlQueue.isEmpty()) {
-                String Url = UrlQueue.take();
-                //通过Url队列中的Url抓取Document，进行Url和文本信息的抓取
-                Document document = Catch.CatchDocument(Url);
-                //数据解析模块返回的数据（含有文本信息以及URL）
-                HashMap<String, ArrayList<String>> DataMap = analysis.AnalysisDocument(document, Url);
+                UrlQueue.stream().parallel().forEach(Url->{
+                    //通过Url队列中的Url抓取Document，进行Url和文本信息的抓取
+                    Document document = Catch.CatchDocument(Url);
+                    //数据解析模块返回的数据（含有文本信息以及URL）
+                    HashMap<String, ArrayList<String>> DataMap = analysis.AnalysisDocument(document, Url);
 
-                //数据处理模块分离出的、只含有URL的集合
-                ArrayList<String> UrlList = new ArrayList<>();
-                try {
-                    UrlList = access.DataAccess(DataMap, sqlFile);
-                } catch (Exception e) {
-                    System.out.println("WormCore--------" + e.getMessage());
-                }
-                //定义迭代器，把抓取到的Url添加到Url队列中
-                Iterator<String> iterator = UrlList.iterator();
+                    //数据处理模块分离出的、只含有URL的集合
+                    ArrayList<String> UrlList = new ArrayList<>();
+                    try {
+                        UrlList = access.DataAccess(DataMap, sqlFile);
+                    } catch (Exception e) {
+                        System.out.println("WormCore--------" + e.getMessage());
+                    }
+                    //定义迭代器，把抓取到的Url添加到Url队列中
+                    Iterator<String> iterator = UrlList.iterator();
+                    while (iterator.hasNext()) {
+                        try {
+                            UrlQueue.put(iterator.next());
+                        } catch (InterruptedException e) {
+                            System.out.println("WormCore--------" + e.getMessage());
+                        }
+                    }
+                    if (UrlQueue.isEmpty()) {
+                        System.out.println("抓取完毕！");
+                        System.exit(1);
+                    }
+                });
 
 
-                while (iterator.hasNext()) {
-                    UrlQueue.put(iterator.next());
-                }
                 //打印URL队列中的URL条数以及队列是否为空
                 /*System.out.println(UrlQueue.size());
                 System.out.println(UrlQueue.isEmpty());*/
                 //为空说明爬取完毕，由于个人技术问题，在抓取完毕之后只能强制退出
-                if (UrlQueue.isEmpty()) {
-                    System.out.println("抓取完毕！");
-                    System.exit(1);
-                }
+
             }
         }
     }
